48999400f498a9cd63f827b74e9e7934d806d743
mengsun <msun@codeaurora.org>
2013-12-11 17:56:42 +0800

    TeleService: Add a controller for in-call proximity sensor (3/3)
    Add the setting for enable proximity sensor or not when calling
    
    ps2: Enable via overlay
    
    Change-Id: I233d8a425bc8daaa224fa4f5493af51eaa4e2433

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -213,6 +213,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private Intent mContactListIntent;
 
+    private static final String BUTTON_PROXIMITY_KEY   = "button_proximity_key";
+
     /** Event for Async voicemail change call */
     private static final int EVENT_VOICEMAIL_CHANGED        = 500;
     private static final int EVENT_FORWARDING_CHANGED       = 501;
@@ -306,6 +308,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     private SwitchPreference mProxSpeaker;
     private SlimSeekBarPreference mProxSpeakerDelay;
     private SwitchPreference mProxSpeakerIncallOnly;
+
+    private SwitchPreference mButtonProximity;
 
     private class VoiceMailProvider {
         public VoiceMailProvider(String name, Intent intent) {
@@ -620,6 +624,16 @@ public class CallFeaturesSetting extends PreferenceActivity
             return true;
         } else if (preference == mButtonVideoCallPictureSelect) {
             startActivity(getVTCallImageSettingsIntent());
+            return true;
+        } else if (preference == mButtonProximity) {
+            int checked = mButtonProximity.isChecked() ? 1 : 0;
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.IN_CALL_PROXIMITY_SENSOR, checked);
+            if (checked == 1) {
+                mButtonProximity.setSummary(R.string.proximity_on_summary);
+            } else {
+                mButtonProximity.setSummary(R.string.proximity_off_summary);
+            }
             return true;
         }
         return false;
@@ -1605,6 +1619,8 @@ public class CallFeaturesSetting extends PreferenceActivity
         super.onCreate(icicle);
         if (DBG) log("onCreate: Intent is " + getIntent());
 
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+
         // Make sure we are running as the primary user.
         if (UserHandle.myUserId() != UserHandle.USER_OWNER) {
             Toast.makeText(this, R.string.call_settings_primary_user_only,
@@ -1639,6 +1655,15 @@ public class CallFeaturesSetting extends PreferenceActivity
         // ACTION_ADD_VOICEMAIL action.
         mShowVoicemailPreference = (icicle == null) &&
                 TextUtils.equals(getIntent().getAction(), ACTION_ADD_VOICEMAIL);
+
+        if (mButtonProximity != null) {
+            if (getResources().getBoolean(R.bool.config_proximity_enable)) {
+                mButtonProximity.setOnPreferenceChangeListener(this);
+            } else {
+                getPreferenceScreen().removePreference(mButtonProximity);
+                mButtonProximity = null;
+            }
+        }
    }
 
     private void initPhoneAccountPreferences() {
@@ -1769,6 +1794,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         mCallRecordingFormat = (ListPreference) findPreference(CALL_RECORDING_FORMAT);
         }
+
+        mButtonProximity = (SwitchPreference) findPreference(BUTTON_PROXIMITY_KEY);
 
         if (mButtonDTMF != null) {
             if (getResources().getBoolean(R.bool.dtmf_type_enabled)) {
@@ -1982,6 +2009,14 @@ public class CallFeaturesSetting extends PreferenceActivity
         // Look up the voicemail ringtone name asynchronously and update its preference.
         new Thread(mVoicemailRingtoneLookupRunnable).start();
         updateBlacklistSummary();
+
+        if (mButtonProximity != null) {
+            boolean checked = Settings.System.getInt(getContentResolver(),
+                    Settings.System.IN_CALL_PROXIMITY_SENSOR, 1) == 1;
+            mButtonProximity.setChecked(checked);
+            mButtonProximity.setSummary(checked ? R.string.proximity_on_summary
+                    : R.string.proximity_off_summary);
+        }
     }
 
     private void updateBlacklistSummary() {
