b7844fa848cad11bd1a8fb402626f05089b8fe9e
Ashish Kumar <akgaurav@google.com>
2023-05-26 14:18:46 +0000

    RESTRICT AUTOMERGE Fixed leak of cross user data in multiple settings.
      - Any app is allowed to receive GET_CONTENT intent. Using this, an user puts back in the intent an uri with data of another user.
      - Telephony service has INTERACT_ACROSS_USER permission. Using this, it reads and shows the deta to the evil user.
    
    Fix: When telephony service gets the intent result, it checks if the uri is from the current user or not.
    
    Bug: b/256591023 , b/256819787
    
    Test: The malicious behaviour was not being reproduced. Unable to import contact from other users data.
    Test2: Able to import contact from the primary user or uri with no user id
    (These settings are not available for secondary users)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:36e10a6d0d7b9efc543f8004729fa85751f4f70d)
    Merged-In: I1e3a643f17948153aecc1d0df9ffd9619ad678c1
    Change-Id: I1e3a643f17948153aecc1d0df9ffd9619ad678c1
    
    Change-Id: If24d76b910fc844c89086d4856bbcf6058f1f76b

--- src/com/android/phone/GsmUmtsCallForwardOptions.java
+++ src/com/android/phone/GsmUmtsCallForwardOptions.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.ContentProvider;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
@@ -14,6 +15,8 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.os.SystemProperties;
+import android.os.Process;
+import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.telephony.CarrierConfigManager;
@@ -448,6 +451,15 @@ public class GsmUmtsCallForwardOptions extends TimeConsumingPreferenceActivity
         }
         Cursor cursor = null;
         try {
+            // check if the URI returned by the user belongs to the user
+            final int currentUser = UserHandle.getUserId(Process.myUid());
+            if (currentUser
+                    != ContentProvider.getUserIdFromUri(data.getData(), currentUser)) {
+
+                Log.w(LOG_TAG, "onActivityResult: Contact data of different user, "
+                        + "cannot access");
+                return;
+            }
             cursor = getContentResolver().query(data.getData(),
                 NUM_PROJECTION, null, null, null);
             if ((cursor == null) || (!cursor.moveToFirst())) {
